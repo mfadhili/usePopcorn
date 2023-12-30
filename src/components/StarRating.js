d97316b193222315d5c2af9c0a7fe8a66a7c1a53
mfadhili <mfadhili@github.com>
2023-12-30 20:02:31 +0300

    start rating feat

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -11,9 +11,14 @@ const starContainer = {
 }
 
 
-/* MAXRATING DESTRUCTURED TO HAVE A DEFAULT VALUE OF 5 INCASE THE VALUE IS NOT PASSED*/
-const StarRating = ({maxRating = 5 , color='#FCC419', size=48}) => {
-    const [rating, setRating] = useState(0);
+/* MAX RATING DESTRUCTURED TO HAVE A DEFAULT VALUE OF 5 IN CASE THE VALUE IS NOT PASSED*/
+/* DEFAULT PROPS HELP AS FALL BACKS*/
+/* CLASS NAME PROPERTY ALLOWS FOR ONE TO EVEN PASS CLASSNAMES AND HAVE MORE CONTROL OVER PROPERTIES LIKE FONT*/
+const StarRating = ({
+                        maxRating = 5 , color='#FCC419',defaultRating=0,
+                        size=48, className='', messages=[], onSetRating
+}) => {
+    const [rating, setRating] = useState(defaultRating);
     const [tempRating, setTempRating] = useState(0);
 
     const textStyle = {
@@ -24,11 +29,12 @@ const StarRating = ({maxRating = 5 , color='#FCC419', size=48}) => {
     }
 
     function handleRating(rating) {
-        setRating(rating)
+        setRating(rating);
+        onSetRating(rating);
     }
 
     return (
-        <div style={containerStyle}>
+        <div style={containerStyle} className={className}>
             <div className="" style={starContainer}>
                 {Array.from({length: maxRating}, (_,i)=>
                     <Star key={i}
@@ -42,7 +48,13 @@ const StarRating = ({maxRating = 5 , color='#FCC419', size=48}) => {
                 )}
             </div>
             {/*SWITCHES TO EMPTY STRING WHEN THE RATING IS FALSY*/}
-            <p style={textStyle}>{tempRating ? tempRating : rating || ""}</p>
+            <p style={textStyle}>
+                { messages.length === maxRating?
+                    messages[tempRating ? tempRating -1 : rating-1]
+                    :
+                    tempRating ? tempRating : rating || ""
+                }
+            </p>
         </div>
     );
 };
@@ -51,8 +63,8 @@ const StarRating = ({maxRating = 5 , color='#FCC419', size=48}) => {
 export function Star({onRate, full, onHoverIn, onHoverOut, color, size}) {
 
     const starStyle ={
-        width: "48px",
-        height: "48px",
+        width: `${size}px`,
+        height: `${size}px`,
         display: "block",
         cursor: "pointer"
     }
@@ -66,8 +78,8 @@ export function Star({onRate, full, onHoverIn, onHoverOut, color, size}) {
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
-                    fill="#000"
-                    stroke="#000"
+                    fill={color}
+                    stroke={color}
                 >
                     <path
                         d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
@@ -78,7 +90,7 @@ export function Star({onRate, full, onHoverIn, onHoverOut, color, size}) {
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke="#000"
+                    stroke={color}
                 >
                     <path
                         strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 //import './index.css';
 import App from './App';
 import StarRating from "./components/StarRating";
 
 
+function Test() {
+    const [movieRating, setMovieRating] = useState(0)
+    return (
+        <div>
+            <StarRating color={"blue"} maxRating={10} onSetRating={setMovieRating}/>
+            <p>This movie was rated {movieRating} stars</p>
+        </div>
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     {/*<App />*/}
-      <StarRating maxRating={5}/>
+      <StarRating
+          maxRating={5} className={"test"}
+          messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+          defaultRating={3}
+      />
+      <StarRating maxRating={5} color={"red"} size={24}/>
+
+      <Test/>
   </React.StrictMode>
 );
 
